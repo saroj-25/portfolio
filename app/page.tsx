b264c954d716fb78f2b3a6d7399c9ae2568dca73
sarosebhandari100 <sarosebhandari100@gmail.com>
2026-07-27 22:34:55 +0545

    nav added

--- app/page.tsx
+++ app/page.tsx
@@ -12,27 +12,14 @@ import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 
 export default function Home() {
-  const [theme, setTheme] = useState<"dark" | "light">("dark");
 
-  useEffect(() => {
-    const root = document.documentElement;
-    if (theme === "light") {
-      root.classList.remove("dark");
-      root.classList.add("light");
-    } else {
-      root.classList.remove("light");
-      root.classList.add("dark");
-    }
-  }, [theme]);
 
-  const toggleTheme = () => {
-    setTheme((prev) => (prev === "dark" ? "light" : "dark"));
-  };
+
 
   return (
     <div className="min-h-screen flex flex-col relative overflow-hidden transition-colors duration-300">
       {/* Fixed Navigation Bar */}
-
+      <Navbar theme={"dark"} />
 
       {/* Main Content Flow */}
       <main className="flex-grow">
